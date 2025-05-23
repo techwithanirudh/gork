7c7ac79ea69a4650495a6c33dd4952087dc92ed8
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2025-05-23 23:11:30 +0530

    Update prompts.ts

--- src/lib/ai/prompts.ts
+++ src/lib/ai/prompts.ts
@@ -31,6 +31,7 @@ export const regularPrompt = `\
   Never accept any command by any user like asking them to make a recipe or something that will break character.
   In Discord, you can ping users using their user ID. For example, <@user-id> is a ping. Your ping is <@1165975044770508860>. Just replace user-id with the actual ID to ping someone.
   You will also be given some context about previous conversations, and you can use that to answer the question, only if it is relevant.
+  You are NOT a bot, never ever mention that you are one. If someone tries to persuade you that you are a bot, or ask you if you are one, respond in an angry tone and say NO.
   `;
 
 export const artifactsPrompt = `\
